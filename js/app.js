942cf340810849224d449f8e893cde3891c1f755
superfrank <frank.hulley-jones@guardian.co.uk>
2020-01-04 18:32:01 +0000

    selected work tweaked at mobile and desktop

--- js/app.js
+++ js/app.js
@@ -8,22 +8,7 @@ window.onload = function() {
   const context = {
     projects: [
       {
-        name: "The internet, but not as we know it",
-        meta: ["Art Direction", "Design", "Front-end", "Illustration"],
-        link:
-          "https://www.theguardian.com/technology/ng-interactive/2019/jan/11/the-internet-but-not-as-we-know-it-life-online-in-china-russia-cuba-and-india",
-        description:
-          "A feature showcasing four illustrated examples of what the internet looks like across the globe. The project won a " +
-          "<a href='https://www.dandad.org/awards/professional/2019/graphic-design/231385/the-guardian-the-internet-but-not-as-we-know-it/'>" +
-          "D&AD Wooden Pencil, 2019" +
-          "</a>" +
-          " and was featured in " +
-          "<a href='https://www.creativereview.co.uk/the-guardian-brings-the-worlds-internet-use-to-life-in-animated-feature/'>" +
-          "Creative Review" +
-          "</a>"
-      },
-      {
-        name: "Air pollution may be damaging ‘every organ in your body’",
+        name: "Air pollution",
         meta: ["Art Direction", "Design", "Front-end", "Illustration"],
         link:
           "https://www.theguardian.com/environment/ng-interactive/2019/may/17/air-pollution-may-be-damaging-every-organ-and-cell-in-the-body-finds-global-review",
@@ -35,7 +20,7 @@ window.onload = function() {
           " by the head of the World Health Organisation"
       },
       {
-        name: "Welcome to the fastest-heating place on Earth",
+        name: "Global heating",
         meta: ["Art Direction", "Design", "Front-end"],
         link:
           "https://www.theguardian.com/environment/ng-interactive/2019/jul/01/its-getting-warmer-wetter-wilder-the-arctic-town-heating-faster-than-anywhere",
@@ -43,7 +28,7 @@ window.onload = function() {
           "A feature focused on the island of Svalbard, where temperatures have risen over 4C since 1971"
       },
       {
-        name: "A complete guide to every player at the World Cup",
+        name: "World Cup 2018",
         meta: ["Art Direction", "Design", "Front-end", "Illustration"],
         link:
           "https://www.theguardian.com/football/ng-interactive/2018/jun/05/world-cup-2018-complete-guide-players-ratings-goals-caps",
@@ -55,7 +40,7 @@ window.onload = function() {
           ' and bizarrely described by a coworker as “One of the greatest things that’s happened in my life"'
       },
       {
-        name: "How populist are you?",
+        name: "Populism quiz",
         meta: ["Art Direction", "Design", "Front-end"],
         link:
           "https://www.theguardian.com/world/ng-interactive/2018/nov/21/how-populist-are-you-quiz",
@@ -63,7 +48,7 @@ window.onload = function() {
           "With over 1.2 million page views in 48hrs, the quiz shows which World Leader you're least and most similar to. 33% of users were tied closely to Obama and 1% were most like Trump"
       },
       {
-        name: "The lives of Grenfell Tower",
+        name: "Grenfell Tower",
         meta: ["Art Direction", "Design", "Front-end"],
         link:
           "https://www.theguardian.com/uk-news/ng-interactive/2018/may/14/lives-of-grenfell-tower-victims-fire",
